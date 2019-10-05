0f56b3daa4cfb86e73d4de900cda6a2b67d85c71
Gustavo Barros <gustavoo.barros07@gmail.com>
2019-10-05 13:19:58 -0300

    Created help screen

--- SmartGuideAPP/src/routes.js
+++ SmartGuideAPP/src/routes.js
@@ -3,11 +3,13 @@ import {createAppContainer, createSwitchNavigator} from 'react-navigation';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Home from './pages/Home';
+import Help from './pages/Help'
 
 export default createAppContainer(
   createSwitchNavigator({
     SignIn,
     SignUp,
-    Home
+    Home,
+    Help
   }),
 );
