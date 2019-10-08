666df506a80cd585d4e5a457b205dd171ad15d37
Gustavo Barros <gustavo.pereira@ihm.com.br>
2019-10-08 11:05:39 -0300

    Created Flatlist for stands list

--- SmartGuideAPP/src/pages/ListStands/index.js
+++ SmartGuideAPP/src/pages/ListStands/index.js
@@ -1,25 +1,49 @@
 import React, {Component} from 'react';
+import {FlatList} from 'react-native';
 
-import logoImage from '~/assets/logo.png';
+import logoImage from '~/assets/stemi_P.png';
 import BackgroundColor from '~/components/Background';
 
 import {
   Container,
+  ScreenTitle,
   Image,
-  Form,
-  FormInput,
-  SubmitButton,
   SignLink,
   SignLinkText,
+  StandTitle,
+  StandDescription,
+  StandContainer,
+  Header,
+  TextContainer,
 } from './styles';
 
 export default function NewStand({navigation}) {
-  let inputs = {};
 
+  let stands = [
+    {key: 'T-Rex', description: 'Tyrannosaurus é um gênero de dinossauros terópodes celurossauros que viveram durante o final do período cretáceo, há aproximadamente 66 milhões de anos, em toda a região que hoje é a América do Norte.'},
+    {key: 'Velociraptor', description: 'Velociraptor (comumente abreviado para "raptor") é um dos gêneros de dinossauros mais conhecidos do público em geral, devido ao seu papel de destaque na série de filmes Jurassic Park.'},
+  ]
   return (
     <BackgroundColor>
       <Container>
-        <Image source={logoImage} />
+        <Header>
+          <ScreenTitle onPress={() => navigation.navigate('Home')}>Lista de bancadas</ScreenTitle>
+        </Header>
+        <FlatList
+          data={stands}
+          renderItem={({item}) => 
+            <StandContainer>
+              <TextContainer>
+                <StandTitle>
+                  {item.key}
+                </StandTitle>
+                <StandDescription>
+                  {item.description}
+                </StandDescription>
+              </TextContainer>
+            </StandContainer>
+          }
+        />
 
         <SignLink onPress={() => navigation.navigate('Home')}>
           <SignLinkText>Voltar</SignLinkText>
