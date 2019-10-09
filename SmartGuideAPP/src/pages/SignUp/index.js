337869163dd335dd7df9761a5a280e39b2a326b4
Eduardo <z.eduardofaria@gmail.com>
2019-10-09 18:47:36 -0300

    showPass method added in SignUp

--- SmartGuideAPP/src/pages/SignUp/index.js
+++ SmartGuideAPP/src/pages/SignUp/index.js
@@ -14,63 +14,78 @@ import {
   LogoText,
 } from './styles';
 
-export default function SignUp({navigation}) {
-  let inputs = {};
+export default class SignUp extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showPass: true,
+      press: false,
+    };
+  }
 
-  return (
-    <BackgroundColor>
-      <Container>
-        <Image source={logoImage} />
-        <LogoText>SmartGuide</LogoText>
+  showPass = () => {
+    const {press, showPass} = this.state;
 
-        <Form>
-          <FormInput
-            icon="user"
-            autoCorrect={false}
-            autoCapitalize="none"
-            placeholder="Nome de usuário"
-            returnKeyType="next"
-            onSubmitEditing={() => {
-              inputs.secondTextInput.focus();
-            }}
-          />
+    this.setState({showPass: !showPass, press: !press});
+  };
 
+  render() {
+    const {navigation} = this.props;
 
-          <FormInput
-            icon="mail"
-            autoCorrect={false}
-            autoCapitalize="none"
-            placeholder="Digite seu e-mail"
-            keyboardType={'email-address'}
-            returnKeyType="next"
-            onSubmitEditing={() => {
-              inputs.thirdTextInput.focus();
-            }}
-            ref={input => {
-              inputs.secondTextInput = input;
-            }}
-          />
+    return (
+      <BackgroundColor>
+        <Container>
+          <Image source={logoImage} />
+          <LogoText>SmartGuide</LogoText>
 
+          <Form>
+            <FormInput
+              icon="user"
+              autoCorrect={false}
+              autoCapitalize="none"
+              placeholder="Nome de usuário"
+              returnKeyType="next"
+              onSubmitEditing={() => {
+                this.props.secondTextInput.focus();
+              }}
+            />
 
-          <FormInput
-            icon="lock"
-            secureTextEntry={true}
-            placeholder="Sua senha secreta"
-            returnKeyType="go"
-            ref={input => {
-              inputs.thirdTextInput = input;
-            }}
-          />
+            <FormInput
+              icon="mail"
+              autoCorrect={false}
+              autoCapitalize="none"
+              placeholder="Digite seu e-mail"
+              keyboardType={'email-address'}
+              returnKeyType="next"
+              onSubmitEditing={() => {
+                this.props.thirdTextInput.focus();
+              }}
+              ref={input => {
+                this.props.secondTextInput = input;
+              }}
+            />
 
-          <SubmitButton onPress={() => navigation.navigate('SignUp')}>
-            Cadastrar
-          </SubmitButton>
-        </Form>
+            <FormInput
+              icon="lock"
+              onShowPass={this.showPass}
+              secureTextEntry={this.state.showPass}
+              placeholder="Sua senha secreta"
+              returnKeyType="go"
+              ref={input => {
+                this.props.thirdTextInput = input;
+              }}
+            />
 
-        <SignLink onPress={() => navigation.navigate('SignIn')}>
-          <SignLinkText>Voltar</SignLinkText>
-        </SignLink>
-      </Container>
-    </BackgroundColor>
-  );
+            <SubmitButton onPress={() => navigation.navigate('SignUp')}>
+              Cadastrar
+            </SubmitButton>
+          </Form>
+
+          <SignLink onPress={() => navigation.navigate('SignIn')}>
+            <SignLinkText>Voltar</SignLinkText>
+          </SignLink>
+        </Container>
+      </BackgroundColor>
+    );
+  }
 }
