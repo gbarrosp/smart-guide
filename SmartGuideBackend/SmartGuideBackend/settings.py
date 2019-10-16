5c5d85b5b82dccc9b9827bd030241c5609c02ab8
Gustavo Barros <gustavoo.barros07@gmail.com>
2019-10-15 21:33:46 -0300

    All models created, image field not available yet

--- SmartGuideBackend/SmartGuideBackend/settings.py
+++ SmartGuideBackend/SmartGuideBackend/settings.py
@@ -54,7 +54,6 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',  
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -67,10 +66,10 @@ MIDDLEWARE = [
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = [
-    'localhost:8000',
+    'http://127.0.0.1:8000',
 ]
 CORS_ORIGIN_REGEX_WHITELIST = [
-    'localhost:8000',
+    'http://127.0.0.1:8000',
 ]
 
 ROOT_URLCONF = 'SmartGuideBackend.urls'
--- SmartGuideBackend/api/views/users.py
+++ SmartGuideBackend/api/views/users.py
@@ -1,4 +1,7 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
@@ -34,3 +37,12 @@ class LogoutUserAPIView(APIView):
         # simply delete the token to force a login
         request.user.auth_token.delete()
         return Response(status=status.HTTP_200_OK)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
