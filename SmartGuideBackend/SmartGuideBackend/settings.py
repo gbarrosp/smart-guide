24c0a6aab20b934c0ebde5b4bc39aac48cd701b1
Gustavo Barros <gustavoo.barros07@gmail.com>
2019-10-21 12:01:20 -0300

    Updated requirements

--- SmartGuideBackend/SmartGuideBackend/settings.py
+++ SmartGuideBackend/SmartGuideBackend/settings.py
@@ -20,10 +20,18 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o$u#@a4%0e+r2!n(j)=1bilgfw@%+hz$yf%gyvjobrt(+jccrb'
+# SECRET_KEY = 'o$u#@a4%0e+r2!n(j)=1bilgfw@%+hz$yf%gyvjobrt(+jccrb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
 
 ALLOWED_HOSTS = []
 
@@ -96,12 +104,12 @@ WSGI_APPLICATION = 'SmartGuideBackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
